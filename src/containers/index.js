11849e03d069d7878f6c8b73c0b691e211062e5e
zhufengnodejs <zhufengnodejs@126.com>
2017-02-07 18:02:55 +0800

    初始化

---
+++ src/containers/index.js
@@ -0,0 +1,15 @@
+import React, { Component, PropTypes } from 'react'
+
+class App extends Component {
+  render(){
+    return (
+      <div>
+        <h1>
+          welcome to 珠峰培训
+        </h1>
+      </div>
+    )
+  }
+}
+
+export default App
